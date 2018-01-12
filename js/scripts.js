dd7e2eec575e9ec2806ee5f8aad2fad2577f163a
johnnymayer <johnnymayer@gmail.com>
2018-01-12 11:02:51 -0800

    Fix js functionality for variable length to string

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+
   $("#userAns").submit(function(event){
     event.preventDefault();
-    var userName = $("#userName").val();
-    var userBday = $("#userBday").val();
-    var ans1 = $("#ans1").val();
-    var ans2 = $("#ans2").val();
-    var ans3 = $("#ans3").val();
-    var ans4 = $("#ans4").val();
-    var ans5 = $("#ans5").val();
-    console.log(userName, userBday, ans1, ans2, ans3, ans4, ans5);
+    var userName = $("#userName").val().length;
+    var userBday = $("#userBday").val().length;
+    var ans1 = $("#ans1").val().length;
+    var ans2 = $("#ans2").val().length;
+    var ans3 = $("#ans3").val().length;
+    var ans4 = $("#ans4").val().length;
+    var ans5 = $("#ans5").val().length;
+    console.log(userName, userBday, ans1, ans2, ans3, ans4, ans5)
   });
 });
