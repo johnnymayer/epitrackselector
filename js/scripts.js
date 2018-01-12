63a5480c3b7a349b205907e76e0f114dd3ee9bc2
Guest User <Guest@Epicodus-9E.local>
2018-01-12 14:13:34 -0800

    Expand js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -9,8 +9,67 @@ $(document).ready(function(){
     var ans3 = $("#ans3").val().length;
     var ans4 = $("#ans4").val().length;
     var ans5 = $("#ans5").val().length;
-    var results = userName+userBday+ans1+ans2+ans3+ans4+ans5;
-    console.log(results)
-    console.log(userName, userBday, ans1, ans2, ans3, ans4, ans5)
+    var results = calcPoints(ans1, ans2, ans3, ans4, ans5);
+    console.log(results);
+    determineTrack(results);
   });
 });
+
+var calcPoints = function() {
+
+  var points = 0;
+
+  if (ans1.length >= 10) {
+    points++;
+  } else if (ans1.length === 5) {
+    points = points+10;
+  } else {
+    points--;
+  };
+
+  if (ans2.length >= 10) {
+    points++;
+  } else if (ans2.length === 5) {
+    points = points+10;
+  } else {
+    points--;
+  };
+
+
+  if (ans3.length > 10) {
+    points++;
+  } else if (ans3.length === 5) {
+    points = points+10;
+  } else {
+    points--;
+  };
+
+  if (ans4.length = 10) {
+    points++;
+  } else if (ans4.length === 5) {
+    points = points+10;
+  } else {
+    points--;
+  };
+
+  if (ans5.length = 10) {
+    points++;
+  } else if (ans5.length === 5) {
+    points = points+10;
+  } else {
+    points--;
+  };
+
+  return points;
+
+};
+
+var determineTrack = function(points) {
+  if (points >= -5 && points < 0) {
+    $("#ruby").show();
+  } else if (points >= 1 && points < 70) {
+    $("#java").show();
+  } else {
+    $("#css").show();
+  }
+};
